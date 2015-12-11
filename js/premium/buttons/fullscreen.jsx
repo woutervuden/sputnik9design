82d14006f876c1c7603cccd89f6694da1df7017c
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2015-12-11 19:59:10 +0100

    Fix rsync file dupes

--- js/premium/buttons/fullscreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default class extends React.Component {
-  render() {
-    return <button className="fullscreen-btn" onClick={this.props.onClick}>
-      Fullscreen
-    </button>
-  }
-}
